1342dcacec6e68e6ed301fb6b4d6f35fa23f183b
Lokijopu <kguo2134@gmail.com>
2018-09-26 13:37:07 -0400

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,12 @@
+public class BankAccount {
+
+	private String name;
+	private int accNum;
+	private double balance;
+	
+	public BankAccount(String str, int accountNumber) {
+		str = name;
+		accountNumber = accNum;
+		balance = 0;
+	}
+}
